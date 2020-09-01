a0d06bd3b2248c2f1f41409a4a17b6005b8063b7
kimHY <guddydcjswo1@gmail.com>
2020-09-01 11:24:42 +0900

    switching visual studio

--- Server/Main.c
+++ Server/Main.c
@@ -1,5 +1,22 @@
 #include "Main.h"
 
 int main(){
-    printf("test");
+    
+    SOCKET sockfd, accept_sock;
+    WSADATA wsaData;
+    SOCKADDR_IN serveraddr, clientaddr;
+
+    if(WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
+        ErrorHandling("WSAStartup() Error");
+    
+    sockfd = socket(PF_INET, SOCK_STREAM, 0);
+
+    if(sockfd == INVALID_SOCKET)
+        ErrorHandling("socket() error");
+
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons(PORT);
+
 }
--- Server/Main.h
+++ Server/Main.h
@@ -1,1 +1,24 @@
-#include <stdio.h>
+#ifndef SERVER_MAIN_H
+#define SERVER_MAIN_H
+
+#include<iostream>
+#include<winSock2.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdexcept>
+
+#include<thread>
+#include<chrono>
+
+using namespace std;
+
+#define IP "127.0.0.1"
+#define PORT 9000
+
+#define BUF_SIZE 4096
+#define QUEUE_SIZE 10
+
+
+void ErrorHandling(string message);
+
+#endif
